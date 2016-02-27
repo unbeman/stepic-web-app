60ec0bf8a04d3e7e01b62900aaf82e98471d7897
unbeman <unbeman@gmail.com>
2016-02-27 22:41:11 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#!/bin/sh
+file_path=/home/box/web
+
+sudo rm /etc/nginx/sites-enabled/*
+sudo ln  -s $file_path/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
