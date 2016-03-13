7a2133c23389ab0afc92c736a894631d0ab8a484
Ann <unbeman@gmail.com>
2016-03-13 00:29:59 +0000

    models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,30 @@
+# coding: utf-8
+import datetime
+
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models.User import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+	
+
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User)
+	likes = models.ManyToManyField(Likes)
+
+	def __unicode__(self):
+		return self.title
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	question = models.ForeignKey(Question)
+	author = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return self.text
