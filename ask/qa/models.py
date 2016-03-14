9d1ee5d26276e7fa3cd3e9ab155799d586c9ba29
unbeman <unbeman@gmail.com>
2016-03-15 00:07:37 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,6 +4,7 @@ import datetime
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 	
@@ -11,17 +12,20 @@ from django.contrib.auth.models import User
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now_add=True)
 	rating = models.IntegerField(default=0)
 	author = models.ForeignKey(User, related_name='question_author')
-	likes = models.ManyToManyField(User, related_name='question_like')
+	likes = models.ManyToManyField(User, related_name='question_like', blank=True)
 
 	def __unicode__(self):
 		return self.title
+	
+	def get_absolute_url(self):
+        	return reverse('question', kwargs={"id": self.id})
 
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now_add=True)
 	question = models.ForeignKey(Question)
 	author = models.ForeignKey(User)
 
