c76d0a361dcc6b682544d69503c3f680d7586053
Ann <unbeman@gmail.com>
2016-03-14 22:28:13 +0000

    new views

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 from .views import test
 
 urlpatterns = [
-    url(r'^$', test, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^login/$', test, name='login'),
     url(r'^signup/$', test, name='signup'),
-    url(r'^question/(?P<id>[^/]+)/$', test, name='question'),
+    url(r'^question/(?P<id>[^/]+)/$', views.question, name='question'),
     url(r'^ask', test, name='ask'),
-    url(r'^popular/$', test, name='popular'),
-    url(r'^new/$', test, name='new'),
+    url(r'^popular/$', views.popular, name='popular'),
+    url(r'^new/$', views.home, name='new'),
 ]
