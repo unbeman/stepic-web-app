6e4db3131c28625bd730936f39162e8880e520e2
unbeman <unbeman@gmail.com>
2016-03-15 00:56:49 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.http import require_GET, require_POST
 
-# Create your views here.
-from django.http import HttpResponse 
+from .models import Question, Answer
+
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+@require_GET
+def home(request):
+    paginator, page = paginate(request, Question.objects.order_by('-added_at'))
+    return render(request, 'qa/home.html', 
+        {
+            'page': page,
+            'paginator': paginator
+        })
+
+@require_GET
+def popular(request):
+    paginator, page = paginate(request, Question.objects.order_by('-rating'))
+    return render(request, 'qa/home.html',
+        {
+         'page': page,
+         'paginator': paginator
+        })
+
+@require_GET
+def question(request, id):
+    question = get_object_or_404(Question, id=id)
+    answer = question.answer_set.all()
+    return render(request, 'qa/question.html',
+        {
+         'question': question,
+         'answers': answers
+        })
