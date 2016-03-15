cc8cc5b1e3ab7a68ddc2098f3867e756aed097bc
ann <unbeman@gmail.com>
2016-03-15 22:50:58 +0000

    views

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.http import require_GET, require_POST
-from django.utils import timezone
-from django.contrib.auth.decorators import login_required
 
 from .models import Question, Answer
 from .utils import paginate
 from .forms import AskForm, AnswerForm
 
+
 def test(request, *args, **kwargs):
-	return HttpResponse('OK')
+    return HttpResponse("OK")
+
 
 @require_GET
 def home(request):
@@ -19,7 +19,7 @@ def home(request):
         'paginator': paginator,
     })
 
-@require_GET
+
 def question(request, id):
     question = get_object_or_404(Question, id=id)
     answers = question.answer_set.all()
@@ -35,23 +35,25 @@ def answer(request):
     form = AnswerForm(request.POST)
     if form.is_valid():
         answer = form.save()
-        return HttpResponseRedirect(answer.question.get_absolute_url())
+        question = answer.question
+        return HttpResponseRedirect(question.get_absolute_url())
 
 
 @require_GET
 def popular(request):
     paginator, page = paginate(request, Question.objects.order_by('-rating'))
-    return render(request, 'qa/home.html', {
+    return render(request, 'qa/index.html', {
         'page': page,
         'paginator': paginator
     })
+
 
 def ask(request):
     if request.method == "POST":
         form = AskForm(request.POST)
         if form.is_valid():
             question = form.save()
-            return HttpResponseRedirect(question.get_absolute_url())
+            return redirect(question)
     else:
         form = AskForm()
     return render(request, 'qa/ask.html', {'form': form})
